abc7eaba4cdeb8fe7d08660c999793b54e6d2615
toddhaugen <todd@haugen.name>
2019-07-19 22:31:20 -0700

    first working return JSON

--- detectBrand/__init__.py
+++ detectBrand/__init__.py
@@ -1,18 +1,12 @@
 import logging
 import os
+import requests
+import json
 import azure.functions as func
 
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
-
-import requests
-# If you are using a Jupyter notebook, uncomment the following line.
-# %matplotlib inline
-#import matplotlib.pyplot as plt
-import json
-#from PIL import Image
-from io import BytesIO
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -49,45 +43,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                                 params=params, json=data)
         response.raise_for_status()
 
-        # The 'analysis' object contains various fields that describe the image. The most
-        # relevant caption for the image is obtained from the 'description' property.
-        analysis = response.json()
-        print(json.dumps(response.json()))
-        #image_caption = analysis["description"]["captions"][0]["text"].capitalize()
-
-        # Set credentials
-        #credentials = CognitiveServicesCredentials('subscriptionKey')
-
-        # Create client
-        #client = ComputerVisionClient(endpoint, credentials)
-
-        # type of prediction
-        #domain = "landmarks"
-
-        # Public domain image of Eiffel tower
-        #url = "https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg"
-
-        # English language response
-        #language = "en"
-
-        #analysis = client.analyze_image_by_domain(domain, url, language)
-
-        #for landmark in analysis.result["landmarks"]:
-        #    print(landmark["name"])
-        #    print(landmark["confidence"])
-
-
-        #url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
-
-        #image_analysis = client.analyze_image(url,visual_features=[VisualFeatureTypes.tags])
-
-        #for tag in image_analysis.tags:
-            #print(tag)
-        
-        # "https://semisupervisedstorage.blob.core.windows.net/pendingevaluation/labledQuaker.jpg"
-
-        return func.HttpResponse(response.json())
-        #return func.HttpResponse(f"Hello {name}!")
+        return func.HttpResponse(json.dumps(response.json()))
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
