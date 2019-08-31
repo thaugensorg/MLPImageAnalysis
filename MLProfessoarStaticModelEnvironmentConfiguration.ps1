11568343a33b2144cf9df8448fea364badde0474
toddhaugen <todd@haugen.name>
2019-08-30 17:46:11 -0700

    fixing cog services sku

--- MLProfessoarStaticModelEnvironmentConfiguration.ps1
+++ MLProfessoarStaticModelEnvironmentConfiguration.ps1
@@ -75,7 +75,7 @@ az cognitiveservices account create `
     --name $cognitiveServicesAccountName `
     --resource-group $modelResourceGroupName `
     --kind ComputerVision `
-    --sku 1 `
+    --sku S1 `
     --location $modelLocation `
     --yes
 
